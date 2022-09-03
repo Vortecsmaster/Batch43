6abffe1746b333d470abd1a5b702fd25a26e42b7
Vortecsmaster <roberto.cerrud@vortecsla.com>
2022-09-03 10:14:46 -0500

    Session 2022-09-02 SignedMinting DeadlineMinting (with homework)

--- NativeTokens/src/Batch43DeadlineMinting.hs
+++ NativeTokens/src/Batch43DeadlineMinting.hs
@@ -1,2 +1,99 @@
-module Batch43DeadlineMinting where
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Batch43SignedMinting where
+
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+
+--ON-CHAIN
+
+{-# INLINABLE signedMintingPolicy #-}
+signedMintingPolicy :: POSIXTime -> PaymentPubKeyHash -> () -> ScriptContext -> Bool
+signedMintingPolicy ptime pkh _ sContext = traceIfFalse "No proper signature" ifSigned &&
+                                           traceIfFalse "Can't no longer mint" deadlineNotReached 
+ 
+   where
+    info :: TxInfo
+    info = scriptContextTxInfo sContext
+
+    ifSigned :: Bool
+    ifSigned = txSignedBy (info) $ unPaymentPubKeyHash pkh
+
+    deadlineNotReached :: Bool
+    deadlineNotReached = True 
+      -- <Homework> (condition of been able to mint BEFORE deadline is reached)
+
+signedpolicy :: POSIXTime -> PaymentPubKeyHash -> Scripts.MintingPolicy
+signedpolicy pkh = mkMintingPolicyScript $ 
+                                   $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . signedMintingPolicy ||])
+                                   `PlutusTx.applyCode`
+                                   PlutusTx.liftCode pkh 
+                                   --Optional: Fix this code so it works!
+curSymbol :: PaymentPubKeyHash -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . signedpolicy
+
+-- OFF-CHAIN
+data MintParams = MintParams
+    { mpTokenName :: !TokenName
+    , mpAmount     :: !Integer
+    } deriving (Generic, ToJSON, FromJSON, ToSchema) 
+
+mint :: MintParams -> Contract w FreeSchema Text ()
+mint mp = do
+          pkh <- Contract.ownPaymentPubKeyHash 
+          let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
+              lookups = Constraints.mintingPolicy (signedpolicy pkh)
+              tx      = Constraints.mustMintValue val
+          ledgerTx <- submitTxConstraintsWith @Void lookups tx
+          void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+          logInfo @String $ printf "We forged %s" (show val)
+
+type FreeSchema = Endpoint "mint" MintParams
+
+endpoints :: Contract () FreeSchema Text ()
+endpoints = mint' >> endpoints
+  where
+    mint' = awaitPromise $ endpoint @"mint" mint
+
+mkSchemaDefinitions ''FreeSchema
+mkKnownCurrencies []
+
+test:: IO ()
+test= runEmulatorTraceIO $ do
+                          h1 <- activateContractWallet (knownWallet 1) endpoints
+                          h2 <- activateContractWallet (knownWallet 2) endpoints
+                          callEndpoint @"mint" h1 $ MintParams
+                                                  { mpTokenName = "Batch43token1",
+                                                    mpAmount = 11000}
+                          callEndpoint @"mint" h2 $ MintParams
+                                                  { mpTokenName = "Batch43token1",
+                                                    mpAmount = 22000}
+                          void $ Emulator.waitNSlots 10
     
--- NativeTokens/src/Batch43SignedMinting.hs
+++ NativeTokens/src/Batch43SignedMinting.hs
@@ -1,2 +1,86 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
 module Batch43SignedMinting where
-    
+
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+
+--ON-CHAIN
+
+{-# INLINABLE signedMintingPolicy #-}
+signedMintingPolicy :: PaymentPubKeyHash -> () -> ScriptContext -> Bool
+signedMintingPolicy pkh _ sContext = txSignedBy (scriptContextTxInfo sContext) $ unPaymentPubKeyHash pkh
+
+signedpolicy :: PaymentPubKeyHash -> Scripts.MintingPolicy
+signedpolicy pkh = mkMintingPolicyScript $ 
+                                   $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . signedMintingPolicy ||])
+                                   `PlutusTx.applyCode`
+                                   PlutusTx.liftCode pkh
+             
+curSymbol :: PaymentPubKeyHash -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . signedpolicy
+
+-- OFF-CHAIN
+data MintParams = MintParams
+    { mpTokenName :: !TokenName
+    , mpAmount     :: !Integer
+    } deriving (Generic, ToJSON, FromJSON, ToSchema) 
+
+mint :: MintParams -> Contract w FreeSchema Text ()
+mint mp = do
+          pkh <- Contract.ownPaymentPubKeyHash 
+          let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
+              lookups = Constraints.mintingPolicy (signedpolicy pkh)
+              tx      = Constraints.mustMintValue val
+          ledgerTx <- submitTxConstraintsWith @Void lookups tx
+          void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+          logInfo @String $ printf "We forged %s" (show val)
+
+type FreeSchema = Endpoint "mint" MintParams
+
+endpoints :: Contract () FreeSchema Text ()
+endpoints = mint' >> endpoints
+  where
+    mint' = awaitPromise $ endpoint @"mint" mint
+
+mkSchemaDefinitions ''FreeSchema
+mkKnownCurrencies []
+
+test:: IO ()
+test= runEmulatorTraceIO $ do
+                          h1 <- activateContractWallet (knownWallet 1) endpoints
+                          h2 <- activateContractWallet (knownWallet 2) endpoints
+                          callEndpoint @"mint" h1 $ MintParams
+                                                  { mpTokenName = "Batch43token1",
+                                                    mpAmount = 11000}
+                          callEndpoint @"mint" h2 $ MintParams
+                                                  { mpTokenName = "Batch43token1",
+                                                    mpAmount = 22000}
+                          void $ Emulator.waitNSlots 10
