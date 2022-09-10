f57974e4e51191194186458af5156b9ac2911bf7
Vortecsmaster <roberto.cerrud@vortecsla.com>
2022-09-10 11:25:13 -0500

    Session 2022-09-10 MathBounty

--- MathBounty/src/Batch43MathBounty.hs
+++ MathBounty/src/Batch43MathBounty.hs
@@ -39,3 +39,91 @@ import qualified Control.Monad.Freer.Extras as Extras
 import PlutusTx.IsData.Class (toBuiltinData)
 
 --ON-CHAIN
+
+data MathBountyDatum = MBD 
+                     { mbInput    :: Integer
+                     , mbDeadline :: POSIXTime }  
+
+PlutusTx.unstableMakeIsData ''MathBountyDatum
+
+{-# INLINABLE mathBountyValidator #-}
+mathBountyValidator :: MathBountyDatum -> Integer -> ScriptContext -> Bool
+mathBountyValidator datum guess sContext = traceIfFalse "Wrong guess!" ((mbInput datum) == guess * guess) &&
+                                           traceIfFalse "Deadline passed!" deadlineNotReached
+    where 
+        info :: TxInfo
+        info = scriptContextTxInfo sContext
+
+        deadlineNotReached :: Bool
+        deadlineNotReached = contains (to $ mbDeadline datum) (txInfoValidRange info)
+                          -- (to $ mbDeadline datum) `contains` (txInfoValidRange info)
+
+data MathBounty
+instance Scripts.ValidatorTypes MathBounty where
+    type instance RedeemerType MathBounty = Integer
+    type instance DatumType MathBounty = MathBountyDatum
+
+bountyValidator :: Scripts.TypedValidator MathBounty
+bountyValidator = Scripts.mkTypedValidator @MathBounty
+                $$(PlutusTx.compile [|| mathBountyValidator ||])
+                $$(PlutusTx.compile [|| wrap ||])
+    where
+        wrap = Scripts.wrapValidator @MathBountyDatum @Integer
+
+
+validator :: Validator  -- Validator {<scripts>}
+validator = Scripts.validatorScript bountyValidator
+
+bountyAddress :: Ledger.Address
+bountyAddress = scriptAddress validator  
+
+-- Equivalent to Cardano-CLI cardano-cli address build --payment-script-file <JSON encoded CBORhex of script> $TESTNET --out-file <some output file>
+
+--OFF-CHAIN
+
+data BountyParams = BP
+                  { bpInput  :: Integer
+                  , bpAmount :: Integer
+                  , bpDeadline :: POSIXTime
+                  } deriving (Generic, FromJSON, ToJSON, Show, ToSchema)
+
+type MathBountySchema =
+        Endpoint "bounty" BountyParams
+    .\/ Endpoint "solution" Integer  
+
+bounty :: BountyParams -> Contract () MathBountySchema Text ()
+bounty (BP input amount deadline) = do 
+                                  let datum = MBD 
+                                              { mbInput    = input
+                                              , mbDeadline = deadline}  
+                                      tx = Constraints.mustPayToTheScript datum (Ada.lovelaceValueOf amount)                                                                     
+                                  ledgerTx <- submitTxConstraints bountyValidator tx
+                                  void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+                                  logInfo @String $ printf "Bounty created of amount = %s" (show amount)
+
+
+solution :: Integer -> Contract () MathBountySchema Text ()
+solution guess = do
+                 now <- currentTime
+                 utxos <- utxosAt bountyAddress
+                 logInfo @String $ printf "The utxos: %s " (show $ Map.toList utxos)
+                 case Map.toList utxos of
+                    []                 -> logInfo @String $ printf "No UTxOs on the Contract!"
+                    (oref,a):xs        -> do
+                                          let lookups = Constraints.otherScript validator  <>
+                                                        Constraints.unspentOutputs (Map.fromList [(oref,a)])
+                                              tx      = Constraints.mustSpendScriptOutput oref (Redeemer $ toBuiltinData guess) <>
+                                                        Constraints.mustValidateIn (to now)
+                                          ledgerTx <- submitTxConstraintsWith @Void lookups tx
+                                          void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+                                          logInfo @String $ printf "Proposed solution was: %s " (show guess)
+
+endpoints :: Contract () MathBountySchema Text ()
+endpoints = awaitPromise (bounty' `select` solution') >> endpoints
+  where 
+    bounty'   = endpoint @"bounty" bounty
+    solution' = endpoint @"solution" solution
+
+mkSchemaDefinitions ''MathBountySchema
+mkKnownCurrencies []
+
